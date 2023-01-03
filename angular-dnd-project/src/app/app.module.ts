6f1119cbbc6750ff4b1918cbad174862b008c021
DaniWitcher <accforjavarush@gmail.com>
2023-01-04 00:13:06 +0300

    header/mainPage

--- angular-dnd-project/src/app/app.module.ts
+++ angular-dnd-project/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './dnd/header/header.component';
+import { MainPageComponent } from './dnd/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
